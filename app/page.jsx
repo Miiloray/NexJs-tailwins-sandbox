a1d69b36c4d5cd3ae36b815aff1ca05ada6dc8e8
Sina Laux <sina.laux@smail.th-koeln.de>
2025-02-13 14:02:47 +0900

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+      <div>
+          <h1>
+              Homepage hello
+          </h1>
+              <p>
+                  lots of text
+              </p>
+
+      </div>
+  );
+}
+
+export default Homepage
